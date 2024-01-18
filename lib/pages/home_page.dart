3e03ac959e7a19f6e3f4233d12013b5dff219750
Aakash Rajbanshi <106716824+aakashx58@users.noreply.github.com>
2024-01-18 17:25:40 +0545

    change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,71 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  late double _deviceHeight, _deviceWidth;
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.red,
-          image: DecorationImage(
-            fit: BoxFit.contain,
-            image: AssetImage("assets/images/moon.png"),
+      body: SafeArea(
+        child: Container(
+          height: _deviceHeight,
+          width: _deviceWidth,
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          color: Colors.amber,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _pageTitle(),
+              _astroImageWidget(),
+              _destinationDropDownWidget(),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.red,
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    List<DropdownMenuItem<String>> _items = [
+      'James Web Station',
+      'Preneure Station',
+    ].map(
+      (e) {
+        return DropdownMenuItem(
+          child: Text(e),
+          value: e,
+        );
+      },
+    ).toList();
+
+    return Container(
+      child: DropdownButton(
+        onChanged: (_) {},
+        items: _items,
       ),
     );
   }
